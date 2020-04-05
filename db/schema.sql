5130949472afa26196191215a13b9773a0bbe2c2
Jason Blake <jblakecodes@gmail.com>
2020-04-05 08:15:14 -0700

    created SQL schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL
+    auto_increment,
+    burger_name VARCHAR
+    (25) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY
+    (id)
+); 
