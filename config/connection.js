7013263803678ea4098ce06b567f96e096121bcf
Jason Blake <jblakecodes@gmail.com>
2020-04-05 10:33:50 -0700

    fixing bugs

--- config/connection.js
+++ config/connection.js
@@ -2,21 +2,25 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    // connection data imported from .env file
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    mysql.createConnection({
+        // connection data imported from .env file
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+    });
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("Error connecting" + err.stack);
-        return;
-    }
-    console.log("Connected as id " + connection.threadId);
-});
+    connection.connect(function (err) {
+        if (err) {
+            console.error("Error connecting" + err.stack);
+            return;
+        }
+        console.log("Connected as id " + connection.threadId);
+    });
 
-module.exports = connection;
+    module.exports = connection;
